0051b5b0fc82ef920fa2c53fb4d356e00ed9de3d
darkchii <darkchii@qq.com>
2021-02-18 17:36:49 +0800

    Update

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -19,7 +19,7 @@ int main()
 	std::cout << (nfa.isMatch("(a|b)+b", "bb") ? "true" : "false") << std::endl;	// true
 	std::cout << (nfa.isMatch("(a|b)*a", "ba") ? "true" : "false") << std::endl;	// true
 	std::cout << (nfa.isMatch("(a|b)*b", "bb") ? "true" : "false") << std::endl;	// true
-	std::cout << (nfa.isMatch("github", "github.com") ? "true" : "false") << std::endl;	// true, because its not a fullmatch, instead to a submatch.
+	std::cout << (nfa.isMatch("github", "github.com") ? "true" : "false") << std::endl;	// true, because its not a fullmatch, instead of a submatch.
 	std::cout << (nfa.isMatch("github.com", "github.com") ? "true" : "false") << std::endl;	// true
 	std::cout << (nfa.isMatch(".*.com", "github.com") ? "true" : "false") << std::endl;	// true
 	
